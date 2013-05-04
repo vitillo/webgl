01a2828714d5037aa0e200302b0ee6ce4979180a
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2013-05-04 00:26:00 -0700

    Tutorial 3 added.

--- framework/framework.js
+++ framework/framework.js
@@ -1,61 +1,106 @@
-function loadShader(gl, id){
-  var shaderScript;
-  var theSource;
-  var currentChild;
-  var shader;
-
-  shaderScript = document.getElementById(id);
-
-  if(!shaderScript){
-    return null;
-  }
-
-  theSource = "";
-  currentChild = shaderScript.firstChild;
-
-  while(currentChild){
-    if(currentChild.nodeType == currentChild.TEXT_NODE){
-      theSource += currentChild.textContent;
+Framework = function(){
+  function initializeWebGL(canvas) {
+    var gl = null;
+    canvas = document.getElementById(canvas);
+    
+    try {
+      gl = canvas.getContext("experimental-webgl");
+    }
+    catch(e) {
+    }
+    
+    if (!gl) {
+      alert("Unable to initialize WebGL. Your browser may not support it.");
     }
 
-    currentChild = currentChild.nextSibling;
+    return gl;
   }
 
-  if(shaderScript.type == "x-shader/x-fragment"){
-    shader = gl.createShader(gl.FRAGMENT_SHADER);
-  }else if(shaderScript.type == "x-shader/x-vertex"){
-    shader = gl.createShader(gl.VERTEX_SHADER);
-  }else{
-    return null;
+  function loadShader(gl, id){
+    var shaderScript;
+    var theSource;
+    var currentChild;
+    var shader;
+
+    shaderScript = document.getElementById(id);
+
+    if(!shaderScript){
+      return null;
+    }
+
+    theSource = "";
+    currentChild = shaderScript.firstChild;
+
+    while(currentChild){
+      if(currentChild.nodeType == currentChild.TEXT_NODE){
+        theSource += currentChild.textContent;
+      }
+
+      currentChild = currentChild.nextSibling;
+    }
+
+    if(shaderScript.type == "x-shader/x-fragment"){
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    }else if(shaderScript.type == "x-shader/x-vertex"){
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    }else{
+      return null;
+    }
+
+    gl.shaderSource(shader, theSource);
+    gl.compileShader(shader);
+
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+      alert("Compile failure in " + id + " shader: " + gl.getShaderInfoLog(shader));
+      return null;
+    }
+
+    return shader;
   }
 
-  gl.shaderSource(shader, theSource);
-  gl.compileShader(shader);
+  function createProgram(list){
+    var prog = gl.createProgram();
 
-  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-    alert("Compile failure in " + id + " shader: " + gl.getShaderInfoLog(shader));
-    return null;
+    for(var i = 0; i < list.length; i++){
+      gl.attachShader(prog, list[i]);
+    }
+
+    gl.linkProgram(prog);
+
+    if(!gl.getProgramParameter(prog, gl.LINK_STATUS)){
+      alert("Shader linker failure: " + gl.getProgramInfoLog(prog));
+    }
+
+    for(var i = 0; i < list.length; i++){
+      gl.detachShader(prog, list[i]);
+    }
+    
+    return prog;
   }
 
-  return shader;
-}
+  window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+           window.webkitRequestAnimationFrame ||
+           window.mozRequestAnimationFrame ||
+           window.oRequestAnimationFrame ||
+           window.msRequestAnimationFrame ||
+           function(callback){
+             return window.setTimeout(callback, 1000/60);
+           };
+  })();
 
-function createProgram(list){
-  var prog = gl.createProgram();
+  window.cancelAnimFrame = (function() {
+    return window.cancelAnimationFrame ||
+           window.webkitCancelAnimationFrame ||
+           window.mozCancelAnimationFrame ||
+           window.oCancelAnimationFrame ||
+           window.msCancelAnimationFrame ||
+           window.clearTimeout;
+  })();
 
-  for(var i = 0; i < list.length; i++){
-    gl.attachShader(prog, list[i]);
-  }
-
-  gl.linkProgram(prog);
-
-  if(!gl.getProgramParameter(prog, gl.LINK_STATUS)){
-    alert("Shader linker failure: " + gl.getProgramInfoLog(prog));
-  }
-
-  for(var i = 0; i < list.length; i++){
-    gl.detachShader(prog, list[i]);
-  }
-  
-  return prog;
-}
+  return {
+    initializeWebGL: initializeWebGL,
+    loadShader : loadShader,
+    createProgram : createProgram
+  };
+}()
